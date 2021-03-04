284bbb4eb9a635dfe4ccbe66c0cbeca619f6daa1
if18b189 <49987011+if18b189@users.noreply.github.com>
2021-03-04 03:55:55 +0100

    - working on the count function - plotting of histograms is not working at this point

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from tkinter import messagebox
 
 import cv2
+import matplotlib
 import numpy as np
 import glob
 import pathlib
 import tkinter as tk
 from PIL import Image, ImageTk
 import os
+from matplotlib import pyplot as plt
+from matplotlib.backends._backend_tk import NavigationToolbar2Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from tkinter import ttk
+
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
 
 
 class ImagePaths:
     def __init__(self, path=os.getcwd() + "\\coins", imageType="jpg"):
         self.imagePaths = glob.glob(os.path.join(path, '*.' + imageType))  # searching for all .jpg files
 
-        print(self.imagePaths)
-        print(os.getcwd())
+        # print(self.imagePaths)
+        # print(os.getcwd())
 
     def fillListBox(self, listBoxObject):
         for imagePath in self.imagePaths:
@@ -27,21 +37,19 @@ class ImagePaths:
 
 class ImageClass:
 
-    def __init__(self, frame, image, colorType="rgb"):
-        self.imageLabel = tk.Label(frame, image=imgtk, compound="top")
-        self.imageLabel.pack(side="right")
-        self.image = image
+    def __init__(self, frame, image, colorType="rgb", description=""):
+        self.image = ImageTk.PhotoImage(image=Image.fromarray(image))
+        self.imageLabel = tk.Label(frame, image=self.image, compound="top", text=description)
+        self.imageLabel.pack(side="right", padx=10, pady=10)
+        self.imageArray = image
         self.colorType = colorType
 
-    def setColorType(self, type="none"):
-        if type != "none":
-            self.colorType == type
-        else:
-            if self.colorType == "rgb":
-                self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+    def setColorType(self):
+        if self.colorType == "rgb":
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
 
-            if self.colorType == "gray":
-                self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        if self.colorType == "gray":
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 
     def setImage(self, imagePath):
         self.image = cv2.imread(imagePath, cv2.IMREAD_COLOR)
@@ -56,10 +64,44 @@ class ImageClass:
         self.imageLabel['image'] = imgtk
         self.imageLabel.photo = imgtk
 
-    def getCurrentImage(self):
-        # TODO: returns the current image
-        print("todo")
+    def getImage(self):
+        return self.image
 
+    def getImageArray(self):
+        return self.imageArray
+
+
+class ImagePlot:
+    def __init__(self):
+        self.image = image
+
+    def showPlot(self, imageArray):
+        self.imageArray = imageArray
+
+        # Toplevel object which will
+        # be treated as a new window
+        newWindow = tk.Toplevel(master)
+
+        # sets the title of the
+        # Toplevel widget
+        newWindow.title("GrayscalePlot")  # add filename - Info
+
+        # sets the geometry of toplevel
+        newWindow.geometry("")  # "" means it will automatically resize
+
+        f = Figure(figsize=(5, 5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        # TODO: implement histogram view
 
 def callbackFileSelection(event):
     selection = event.widget.curselection()
@@ -67,8 +109,33 @@ def callbackFileSelection(event):
     originalImage.setImage(lbImagePaths.getPath(selection[0]))
     grayImage.setImage(lbImagePaths.getPath(selection[0]))
 
+
+def openPlot():
+    grayImagePlot.showPlot(grayImage.getImageArray())
+
+
 def countCoins():
-    print("hello")
+    self.imageArray = imageArray
+
+    # Toplevel object which will
+    # be treated as a new window
+    countWindow = tk.Toplevel(master)
+
+    # sets the title of the
+    # Toplevel widget
+    countWindow.title("Counting Coins")  # add filename - Info
+
+    # sets the geometry of toplevel
+    countWindow.geometry("")  # "" means it will automatically resize
+
+
+    img = grayImage.getImage()
+
+    ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+    ret, thresh3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
+    ret, thresh4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
+    ret, thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
 
 
 if __name__ == '__main__':
@@ -83,12 +150,10 @@ if __name__ == '__main__':
 
     image = cv2.imread('.\\coins\\coinb_05.JPG', cv2.IMREAD_COLOR)
 
-    imgtk = ImageTk.PhotoImage(image=Image.fromarray(image))
+    originalImage = ImageClass(rightFrame, image, "rgb", "RGB")  # creating image object in rgb(default)
+    grayImage = ImageClass(rightFrame, image, "gray", "GRAYSCALE")  # creating image objekt in grayscale
 
-    originalImage = ImageClass(rightFrame, imgtk)
-    grayImage = ImageClass(rightFrame, imgtk, "gray")
-
-
+    grayImagePlot = ImagePlot()
 
     lbFileSelection = tk.Listbox(master, width=30)  # creating a listbox
     lbFileSelection.bind("<<ListboxSelect>>",
@@ -99,7 +164,10 @@ if __name__ == '__main__':
     lbImagePaths = ImagePaths()
     lbImagePaths.fillListBox(lbFileSelection)
 
-    countCoins = tk.Button(master, text='count', width=15, height=2, command=countCoins)
+    countCoins = tk.Button(master, text='Count', width=15, height=2, command=countCoins)
+    countCoins.pack(side="bottom", padx=10, pady=10)
+
+    countCoins = tk.Button(master, text='Plot', width=15, height=2, command=openPlot)
     countCoins.pack(side="bottom", padx=10, pady=10)
 
     master.mainloop()  # window mainloop
